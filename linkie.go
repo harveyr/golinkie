820484c3dbb328d919daa8d3c924dc830f816249
Harvey Rogers <harveyr@gmail.com>
2013-10-09 16:51:55 -0700

    Working

--- linkie.go
+++ linkie.go
@@ -1,14 +1,34 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"regexp"
-	"net/http"
 	"flag"
-	"net/url"
+	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"regexp"
 )
+
+type TestResult struct {
+	Url        string
+	StatusCode int
+}
+
+var colors = map[string]string{
+    "header": "\033[95m",
+    "blue": "\033[94m",
+    "green": "\033[92m",
+    "yellow": "\033[93m",
+    "red": "\033[91m",
+    "bold": "\033[1m",
+    "end": "\033[0m",
+}
+
+func printColor(s string, color string) {
+	formatted := fmt.Sprintf("%s%s%s", colors[color], s, colors["end"])
+	fmt.Println(formatted)
+}
 
 func fetchHtml(inputUrl string) string {
 	parsedUrl, err := url.Parse(inputUrl)
@@ -18,6 +38,7 @@ func fetchHtml(inputUrl string) string {
 	if !parsedUrl.IsAbs() {
 		log.Fatal("Url must be absolute")
 	}
+
 	resp, err := http.Get(parsedUrl.String())
 	defer resp.Body.Close()
 	if err != nil {
@@ -26,16 +47,20 @@ func fetchHtml(inputUrl string) string {
 	if resp.StatusCode != 200 {
 		log.Fatalf("Failed to reach %s (%s)", parsedUrl, resp.StatusCode)
 	}
+
 	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Failed to read response body")
+	}
 	return string(body)
 }
 
 func findLinkUrls(inputHtml string, parentUrlRaw string) []string {
 	parentUrl, _ := url.Parse(parentUrlRaw)
 
-	regex := regexp.MustCompile(`src="(\S+)"`)
+	regex := regexp.MustCompile(`(?:src|href)="(\S+)"`)
 	submatches := regex.FindAllStringSubmatch(inputHtml, -1)
-	
+
 	linkUrls := make([]string, len(submatches))
 	for i, v := range submatches {
 		linkUrl, _ := url.Parse(v[1])
@@ -47,38 +72,43 @@ func findLinkUrls(inputHtml string, parentUrlRaw string) []string {
 	return linkUrls
 }
 
-func testLinkUrl(linkUrl string, c chan map[string]int) {
+func testLinkUrl(linkUrl string, c chan TestResult) {
 	resp, err := http.Get(linkUrl)
-	result := make(map[string]int)
+	var code int
 	if err != nil {
-		result[linkUrl] = -1
+		code = -111
 	} else {
-		result[linkUrl] = resp.StatusCode
+		code = resp.StatusCode
 	}
+	result := TestResult{linkUrl, code}
 	c <- result
 }
 
-func testLinkUrls(linkUrls []string) (chan map[string]int, int) {
-	c := make(chan map[string]int)
+func testLinkUrls(linkUrls []string) (chan TestResult) {
+	c := make(chan TestResult)
 	for _, linkUrl := range linkUrls {
 		go testLinkUrl(linkUrl, c)
 	}
-	return c, len(linkUrls)
+	return c
 }
 
 func main() {
+	printColor("--- Linkie Time! ---", "header")
 	var inputUrl string
 	flag.StringVar(&inputUrl, "url", "", "Provide a valid url")
 	flag.Parse()
-	fmt.Println(inputUrl)
 
 	responseHtml := fetchHtml(inputUrl)
-	log.Printf("Fetched %d characters", len(responseHtml))
+	printColor(inputUrl, "bold")
 
 	linkUrls := findLinkUrls(responseHtml, inputUrl)
-	c, count := testLinkUrls(linkUrls)
-	for i := 0; i < count; i++ {
-		result := <- c
-		log.Print("result: ", result)
+	c := testLinkUrls(linkUrls)
+	for i := 0; i < len(linkUrls); i++ {
+		result := <-c
+		color := "green"
+		if result.StatusCode != 200 {
+			color = "red"
+		}
+		printColor(fmt.Sprintf("[%d] %s", result.StatusCode, result.Url), color)
 	}
 }
